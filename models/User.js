cacbde6e311187220df99dafd9b4fc172b008975
cam-cole <camcole065@gmail.com>
2022-07-25 19:31:50 -0500

    finished user model

--- models/User.js
+++ models/User.js
@@ -7,6 +7,26 @@ const UserSchema = new Schema(
             required: true,
             trim: true
         },
-        
+        email: {
+            type: String,
+            required: true,
+        },
+        thoughts: {
+
+        },
+        friends: {
+
+        }
     }
-)
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce(
+        (total, friend) => total + friend.length + 1,
+        0
+    );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
